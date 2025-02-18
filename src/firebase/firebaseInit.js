473fd8924a614ff1781671d72e4a655ef5b68bcf
Harendrasinh <haren111990@gmail.com>
2025-02-18 22:28:05 +0530

    update2

--- src/firebase/firebaseInit.js
+++ src/firebase/firebaseInit.js
@@ -7,10 +7,10 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId:process.env.REACT_APP_FIREBASE_APP_ID,
 };
@@ -19,4 +19,3 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export const auth = getAuth(app);
-console.log(process.env.REACT_APP_FIREBASE_API_KEY);
